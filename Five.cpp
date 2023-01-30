3935f5bdc6706cdfe35b9fdb6af85e19491c9439
Ashutosh Maurya <dev26@bytescrum.com>
2023-01-30 18:01:42 +0530

    ...cpp

---
+++ Five.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string X,Y;
+
+    cin>>X>>Y;
+
+    if(X>Y){
+        cout<<Y<<endl;
+    }else{
+        cout<<X<<endl;
+    }
+
+
+}
---
+++ Four.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int main() {
+
+//corssed limits we have to change log
+    long long A,B,C,D;
+    cin>>A>>B>>C>>D;
+    if(pow(A,B) > pow(C,D) ){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+    
+
+}
