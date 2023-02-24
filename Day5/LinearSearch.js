05ebb64f0549de76f079d551a28c55346b367db1
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-02-24 20:29:52 +0530

    linear seatch

--- Day5/LinearSearch.js
+++ Day5/LinearSearch.js
@@ -8,3 +8,15 @@ function search(arr, x) {
 
   return -1;
 }
+
+//linear search
+//if array is unshorted we have to implement linear search
+
+function linearSearch(arr, x) {
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] == x) {
+      return i;
+    }
+  }
+  return -1;
+}
